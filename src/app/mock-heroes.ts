a1e128c5b863a43e5b01126475be49e92bcf21fe
Walter <fulopwalterofficial@gmail.com>
2019-12-03 13:11:33 +0100

    Ready with listing the heroes.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Loki'},
+    { id: 12, name: 'Ironman'},
+    { id: 13, name: 'Captain America'},
+    { id: 14, name: 'Visio'},
+    { id: 15, name: 'Hulk'},
+    { id: 16, name: 'Romanov'},
+    { id: 17, name: 'Hawkeye'},
+    { id: 18, name: 'Spiderman'},
+    { id: 19, name: 'Black Panther'},
+    { id: 20, name: 'Hawk'},
+]
